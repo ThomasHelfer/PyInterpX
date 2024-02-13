9368e32314766545acb7ecd10bb5f1cf76a098de
ThomasHelfer <thomashelfer@live.de>
2024-02-13 11:41:06 -0500

    slight change in naming

--- setup.py
+++ setup.py
@@ -6,28 +6,31 @@ def readme():
         return f.read()
 
 
-def read_requirements():
-    with open("requirements.txt") as f:
-        return f.read().splitlines()
-
-
 setup(
     name="interpolation_higher_order_3d_torch",
-    version="0.1",
+    version="0.1.1",
     description="A highly performant, GPU compatible package for higher order interpolation in PyTorch",
     long_description=readme(),
+    long_description_content_type='text/markdown',
     classifiers=[
         "Development Status :: 3 - Alpha",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
-        "Topic :: Pytorch :: Interpolation :: Machine learning",
     ],
     keywords="Machine learning, Pytorch, Interpolation, Higher order interpolation, 3D interpolation, GPU compatible",
     author="ThomasHelfer",
     author_email="thomashelfer@live.de",
     license="MIT",  # Updated to MIT License
     packages=find_packages(exclude=["tests"]),
-    install_requires=read_requirements(),
+    install_requires=[
+        "torch",
+        "black",
+        "pre-commit",
+        "pytest",
+        "numpy",
+        "tqdm",
+        "matplotlib",
+    ],
     python_requires=">=3.8",
     include_package_data=True,
     zip_safe=False,
